f6c9c08279ce9c62ba1bccc531d709907ecf7f43
realbrodiwhite <149413096+realbrodiwhite@users.noreply.github.com>
2024-01-26 05:01:21 +0000

    yee

--- admin/index.js
+++ admin/index.js
@@ -1,64 +1,16 @@
-const AdminJS = require('adminjs')
-const adminJs = new AdminJS()
-const AdminJSExpress = require('@adminjs/express')
-const AdminJSSequelize = require('@adminjs/sequelize')
-
-AdminJS.registerAdapter(AdminJSSequelize)
-const admin = adminJs.buildAuthenticated{
-  resources: {
-    User: {
-      properties: {
-        email: {
-          isVisible: {
-            show: true,
-          },
-        },
-      },
-    },
-  },
-}
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-const useradmin = adminJs.build{
-  resources: {
-    resource: require('./models/User'),
-    options: {
-      properties: {
-        email: {
-          isVisible: false,
-        },
-        password: {
-          isVisible: false,
-        },
-      },
-    },
-  },
-  
-  
-  
-const { Database, Resource } = require('adminjs')
-
-// Configure AdminJS options
-const adminJsOptions = {
-  databases: [], // Add databases here
-  rootPath: '/admin',
-  resources: [{ resource: Resource, options: { parent: { name: 'Monorepo' } } }], // Add resources here
-  dashboard: {
-    handler: async () => {
-      // Custom dashboard handler
-    },
-    component: AdminJS.bundle('./my-dashboard-component')
-  }
-}
-
+const AdminJS = require("adminjs");
+const AdminJSExpress = require("@adminjs/express");
+const AdminJSSequelize = require("@adminjs/sequelize");
+const express = require("express");
+const app = express();
+const { Database, Resource } = require("adminjs");
 // Initialize AdminJS with the options
-const admin = new AdminJS(adminJsOptions)
+const admin = new AdminJS(adminJsOptions);
 
 // Build and use a router which will handle all AdminJS routes
-const adminRouter = AdminJSExpress.buildRouter(admin)
-app.use(adminJsOptions.rootPath, adminRouter)
+const adminRouter = AdminJSExpress.buildRouter(adminJs.build);
+app.use(adminJsOptions.rootPath, adminRouter);
 
 // Start the server
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
